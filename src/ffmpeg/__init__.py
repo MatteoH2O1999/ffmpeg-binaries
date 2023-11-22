e30db00df68622c60456c28e2bc588e0667946dc
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-11-22 13:39:26 +0100

    Move run method in executable and add first code

--- src/ffmpeg/__init__.py
+++ src/ffmpeg/__init__.py
@@ -1,18 +1,22 @@
+import warnings
+
 from .executable import add_to_path, get_executable_path
 
 __version__ = "0.0.1"
 
 FFMPEG_PATH = get_executable_path()
-
-
-def run() -> None:
-    import subprocess
-    import sys
-
-    subprocess.run([FFMPEG_PATH, *sys.argv[1:]])
+if FFMPEG_PATH is None:
+    warnings.warn(
+        "ffmpeg binaries not found. Please call ffmpeg.init() to download them."
+    )
 
 
 def run_as_ffmpeg(command: str) -> None:
     import subprocess
 
     subprocess.run([FFMPEG_PATH, command])
+
+
+def init() -> None:
+    global FFMPEG_PATH
+    FFMPEG_PATH = get_executable_path(ensure_binaries=True)
--- src/ffmpeg/executable.py
+++ src/ffmpeg/executable.py
@@ -1,11 +1,29 @@
+import os
 import pathlib
+import warnings
 
-from .internals import get_binaries
+from .internals import download_binaries, get_binaries
 
 
-def get_executable_path() -> pathlib.Path:
-    return get_binaries()
+def get_executable_path(ensure_binaries: bool = False) -> pathlib.Path | None:
+    binaries_path = get_binaries()
+    if binaries_path is None and ensure_binaries:
+        warnings.warn("ffmpeg binaries not found. Will download binaries now.")
+        download_binaries()
+        binaries_path = get_binaries()
+        if binaries_path is None:
+            raise RuntimeError("Could not download correct ffmpeg binaries.")
+        return binaries_path
+    return binaries_path
 
 
 def add_to_path() -> None:
-    pass
+    path_to_add = get_executable_path(ensure_binaries=True)
+    os.environ["PATH"] = f"{str(path_to_add)}{os.pathsep}{os.environ['PATH']}"
+
+
+def _run() -> None:
+    import subprocess
+    import sys
+
+    subprocess.run([str(get_executable_path(ensure_binaries=True)), *sys.argv[1:]])
