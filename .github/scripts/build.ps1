fcda71bed94c0e5e0b455fc682ae2e6af7fda442
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2025-03-28 01:25:28 +0100

    Delete build folder between steps

--- .github/scripts/build.ps1
+++ .github/scripts/build.ps1
@@ -4,9 +4,14 @@ Set-Location -Path $root
 $originalPyproject = Join-Path -Path $root -ChildPath "pyproject.toml"
 $renamedOriginalPyproject = Join-Path -Path $root -ChildPath "old-pyproject.toml"
 $namespacePyproject = Join-Path -Path $root -ChildPath "pyproject-namespace.toml"
+$buildFolder = Join-Path -Path $root -ChildPath "build"
 
 Write-Host "Removing egg-info folders..."
 Get-ChildItem $root -Recurse -Filter "*egg-info" | Remove-Item -Confirm:$false -Recurse
+if (Test-Path -Path $buildFolder) {
+    Write-Host "Removing build folder..."
+    Remove-Item -Confirm:$false -Recurse -Path $buildFolder
+}
 
 Write-Host "Building original package..."
 Write-Host "$root> python -m build -o ./dist $args" -ForegroundColor Blue
@@ -18,6 +23,10 @@ Rename-Item -Path $namespacePyproject -NewName $originalPyproject
 
 Write-Host "Removing egg-info folders..."
 Get-ChildItem $root -Recurse -Filter "*egg-info" | Remove-Item -Confirm:$false -Recurse
+if (Test-Path -Path $buildFolder) {
+    Write-Host "Removing build folder..."
+    Remove-Item -Confirm:$false -Recurse -Path $buildFolder
+}
 
 Write-Host "Building namespace package..."
 Write-Host "$root> python -m build -o ./dist $args" -ForegroundColor Blue
@@ -26,3 +35,10 @@ python -m build -o ./dist $args
 Write-Host "Restoring project files..."
 Rename-Item -Path $originalPyproject -NewName $namespacePyproject
 Rename-Item -Path $renamedOriginalPyproject -NewName $originalPyproject
+
+Write-Host "Removing egg-info folders..."
+Get-ChildItem $root -Recurse -Filter "*egg-info" | Remove-Item -Confirm:$false -Recurse
+if (Test-Path -Path $buildFolder) {
+    Write-Host "Removing build folder..."
+    Remove-Item -Confirm:$false -Recurse -Path $buildFolder
+}
